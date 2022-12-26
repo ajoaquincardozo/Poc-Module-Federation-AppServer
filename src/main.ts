13225e2e69814cd16389997ef8d5ba03633c7bb4
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-26 11:31:17 -0300

    Se cambia implementacion de config de cors

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedUrls = [
+    'https://poc-module-federation-app-shell.vercel.app',
+    // 'https://poc-module-federation-products.vercel.app',
+    'http://localhost:8080',
+    'http://localhost:8082',
+    'http://127.0.0.1:8082',
+    // 'http://localhost:3000',
+    "https://poc-iframe-app-shell.vercel.app"
+  ];
+
   app.enableCors({
-    // allowedHeaders: 'Content-Type, Authorization',
-    origin: [
-      'https://poc-module-federation-app-shell.vercel.app',
-      // 'https://poc-module-federation-products.vercel.app',
-      'http://localhost:8080',
-      'http://localhost:8082',
-      // 'http://localhost:3000',
-      "https://poc-iframe-app-shell.vercel.app"
-    ],
-    // methods: ["GET","OPTIONS","PATCH","DELETE","POST","PUT"]
+    origin: (origin, callback) => {
+      if (!origin || allowedUrls.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
   });
   await app.listen(8081);
 }
